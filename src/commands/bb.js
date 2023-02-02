db2cea4270aac99284b2301798bfe31920aa0d69
Renzo Telenta <renzotelenta@gmail.com>
2023-02-01 22:05:58 -0500

    add filter enabled audios

--- src/commands/bb.js
+++ src/commands/bb.js
@@ -13,7 +13,7 @@ const audios = async () => {
 }
 
 const addLog = async (audio_id) => {
-  await supabase.from("usage").insert([{ audio_id }])
+  await supabase.from("usage").insert([{ audio_id }]).eq("enabled", true)
 }
 
 const data = async () => {
